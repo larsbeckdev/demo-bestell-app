4cc16f1828f5aa674c91eef7f625b3d76ce96b1e
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-22 16:24:56 +0000

    Add Tailwind CSS integration to the project

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
