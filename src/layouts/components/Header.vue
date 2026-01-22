f23b271dcda20b7c2a4d4d66126d1aa260528603
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-22 18:09:58 +0000

    Add mobile menu button and improve desktop menu structure in the header component

--- src/layouts/components/Header.vue
+++ src/layouts/components/Header.vue
@@ -13,6 +13,7 @@ import ThemeController from '@/components/ui/theme/ThemeController.vue'
       <!-- Right -->
       <div class="navbar-end">
         <div class="flex items-center gap-4">
+          <!-- Desktop Menu -->
           <ul class="menu menu-horizontal px-1 items-center">
             <li><a>Link</a></li>
             <li>
@@ -25,6 +26,23 @@ import ThemeController from '@/components/ui/theme/ThemeController.vue'
               </details>
             </li>
           </ul>
+
+          <!-- Mobile Menu Button -->
+          <button class="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block h-5 w-5 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
 
           <!-- Theme -->
           <ThemeController />
