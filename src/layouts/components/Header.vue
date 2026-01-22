212563296c02296ded2b750d311c3fb1ebdc7f0e
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-22 18:34:59 +0000

    Update header layout styles by adjusting padding values

--- src/layouts/components/Header.vue
+++ src/layouts/components/Header.vue
@@ -3,8 +3,8 @@ import ThemeController from '@/components/ui/theme/ThemeController.vue'
 </script>
 
 <template>
-  <header class="w-full bg-primary">
-    <div class="navbar shadow-sm px-4">
+  <header class="w-full bg-primary px-4">
+    <div class="navbar shadow-sm px-0">
       <!-- Left -->
       <div class="navbar-start">
         <a class="text-xl"
