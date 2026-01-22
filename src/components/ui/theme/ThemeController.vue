7cd0b9fb6501c79ce0cd5568498ad251f7893e0b
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-22 20:50:08 +0000

    Update default theme initialization and selection to "bestellapp"

--- src/components/ui/theme/ThemeController.vue
+++ src/components/ui/theme/ThemeController.vue
@@ -4,7 +4,7 @@ import { THEMES } from './data.js'
 import { initTheme, applyTheme, setStoredTheme, getStoredTheme } from './utils.js'
 import { PaintBucket } from 'lucide-vue-next'
 
-const selected = ref('light')
+const selected = ref('bestellapp')
 
 function setTheme(theme) {
   selected.value = theme
@@ -13,7 +13,7 @@ function setTheme(theme) {
 }
 
 onMounted(() => {
-  const theme = initTheme('light')
+  const theme = initTheme('bestellapp')
   selected.value = theme
 
   const stored = getStoredTheme()
