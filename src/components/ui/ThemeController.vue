401145ed0b764582566a0a9763742a5325f59bff
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-22 17:49:23 +0000

    Update ThemeController dropdown alignment by adding dropdown-end class

--- src/components/ui/ThemeController.vue
+++ src/components/ui/ThemeController.vue
@@ -14,7 +14,10 @@
         <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
       </svg>
     </div>
-    <ul tabindex="-1" class="dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl">
+    <ul
+      tabindex="-1"
+      class="dropdown-content dropdown-end bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl"
+    >
       <li>
         <input
           type="radio"
