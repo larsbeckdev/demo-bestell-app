8abbb87084b00d03e98fe3b20072bec1f2f50e73
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-23 09:51:06 +0000

    Update hero image reference in OrderLayout component

--- src/layouts/order/OrderLayout.vue
+++ src/layouts/order/OrderLayout.vue
@@ -4,7 +4,7 @@ import Footer from '@/layouts/components/Footer.vue'
 import OrderContent from '@/layouts/components/OrderContent.vue'
 import Basket from '@/layouts/components/Basket.vue'
 
-const heroUrl = new URL('@/assets/images/hero-3.webp', import.meta.url).href
+const heroUrl = new URL('@/assets/images/hero-7.webp', import.meta.url).href
 const logoUrl = new URL('@/assets/images/Burger.png', import.meta.url).href
 </script>
 
