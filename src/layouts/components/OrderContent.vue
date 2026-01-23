1c26da6502d780aa64e6cd73c3d4cb0b8e99f80f
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-23 11:48:45 +0000

    Remove unused OrderPage component and its reference from OrderContent component

--- src/layouts/components/OrderContent.vue
+++ src/layouts/components/OrderContent.vue
@@ -1,11 +1,10 @@
 <script setup>
-import OrderPage from '@/pages/OrderPage.vue'
 </script>
 
 <template>
   <div class="rounded-lg bg-base-200 p-4 h-full">
     <div class="mb-4 flex items-center justify-between">
-      <OrderPage />
+     
     </div>
   </div>
 </template>
