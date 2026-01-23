a6a24403938beb41ffd4b7390ff677ea52e93332
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-23 10:12:09 +0000

    Update background color class in OrderContent component

--- src/layouts/components/OrderContent.vue
+++ src/layouts/components/OrderContent.vue
@@ -3,7 +3,7 @@ import OrderPage from '@/pages/OrderPage.vue'
 </script>
 
 <template>
-  <div class="rounded-lg bg-base-300 p-4 h-full">
+  <div class="rounded-lg bg-base-200 p-4 h-full">
     <div class="mb-4 flex items-center justify-between">
       <OrderPage />
     </div>
