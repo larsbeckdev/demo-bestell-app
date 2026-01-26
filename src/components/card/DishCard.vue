c7d8a66e31a931c17637e05e13b207a32e90a92c
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-26 11:06:50 +0000

    Update button hover styles and remove redundant class from DishCard button

--- src/components/card/DishCard.vue
+++ src/components/card/DishCard.vue
@@ -39,7 +39,7 @@ const emit = defineEmits(['add', 'remove'])
 
         <button
           v-if="dish.quantity > 0"
-          class="btn btn-circle btn-sm btn-soft bg-base-300 border-0"
+          class="btn btn-circle btn-sm bg-base-300"
           type="button"
           aria-label="Remove from basket"
           @click="emit('remove', dish)"
