73957a36b8e70774f4879ab4eada541b788ecf90
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-26 12:06:04 +0000

    Add HeroSection component to HomePage and update template structure

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,11 @@
-<script setup></script>
+<script setup>
+import { HeroSection } from '@/sections/hero/HeroSection.vue'
+</script>
 
 <template>
   <div class="h-screen flex items-center justify-center flex-col space-y-4">
-    <h1 class="text-3xl font-bold underline">
-      Startseite
-    </h1>
+    <HeroSection />
+    <h1 class="text-3xl font-bold underline">Startseite</h1>
     <router-link to="/bestellen" class="btn btn-primary">Menü anzeigen</router-link>
   </div>
 </template>
