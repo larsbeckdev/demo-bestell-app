22c9add2f9d266786d0aa5ccfbd31e507a3856f6
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-26 12:03:41 +0000

    Update HomePage layout with centered content, welcome message, and navigation link

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,13 @@
 <script setup></script>
 
 <template>
-  <div class="h-screen"></div>
+  <div class="h-screen flex items-center justify-center flex-col">
+    <h1 class="text-3xl font-bold underline">
+      Startseite
+    </h1>
+    <p class="mt-4">Willkommen auf der Startseite!</p>
+    <router-link to="/bestellen" class="btn btn-primary text-blue-500 hover:underline">Erfahre mehr über uns</router-link>
+  </div>
 </template>
 
 <style scoped></style>
