3ec51b9af90f2d73bc5ac35a525a9a1af45882a5
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-26 12:04:28 +0000

    Update HomePage layout spacing and add text to the router link

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,11 @@
 <script setup></script>
 
 <template>
-  <div class="h-screen flex items-center justify-center flex-col">
+  <div class="h-screen flex items-center justify-center flex-col space-y-4">
     <h1 class="text-3xl font-bold underline">
       Startseite
     </h1>
-    <p class="mt-4">Willkommen auf der Startseite!</p>
-    <router-link to="/bestellen" class="btn btn-primary"></router-link>
+    <router-link to="/bestellen" class="btn btn-primary">Menü anzeigen</router-link>
   </div>
 </template>
 
