e66b2c3b57faf939bc9e9a217be310efcdc006ef
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-27 14:45:57 +0000

    Update DishCard button class from btn-wide to btn-block for quantity display

--- src/components/card/DishCard.vue
+++ src/components/card/DishCard.vue
@@ -33,7 +33,7 @@ const emit = defineEmits(['add', 'remove'])
       <div class="font-semibold whitespace-nowrap">{{ Number(dish.price).toFixed(2) }}€</div>
 
       <div class="flex items-center gap-2">
-        <span v-if="dish.quantity > 0" class="rounded-full btn px-3 text-sm btn-soft btn-wide ">
+        <span v-if="dish.quantity > 0" class="rounded-full btn px-3 text-sm btn-soft btn-block ">
           {{ dish.quantity }}
         </span>
 
