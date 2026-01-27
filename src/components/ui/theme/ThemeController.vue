5a8b08755a0ac7fd24ae5803bd2bbdb3e636d1c0
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-27 06:35:13 +0000

    Fix extra whitespace in button class attribute

--- src/components/ui/theme/ThemeController.vue
+++ src/components/ui/theme/ThemeController.vue
@@ -23,7 +23,7 @@ onMounted(() => {
 
 <template>
   <div class="dropdown dropdown-end">
-    <button type="button" tabindex="0" class="btn btn-sm btn-warning gap-2 " aria-label="Theme auswählen">      <!-- Palette Icon -->
+    <button type="button" tabindex="0" class="btn btn-sm btn-warning gap-2" aria-label="Theme auswählen">      <!-- Palette Icon -->
       <PaintBucket class="h-4 w-4 opacity-80" />
 
       <!-- <span class="font-medium">Theme</span> -->
