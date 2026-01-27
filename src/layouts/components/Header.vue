eb088126ac88468aabe63244c77302215d48d199
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-27 06:41:39 +0000

    Add empty line for formatting consistency in Header component

--- src/layouts/components/Header.vue
+++ src/layouts/components/Header.vue
@@ -70,6 +70,7 @@ import ThemeController from '@/components/ui/theme/ThemeController.vue'
               <details>
                 <summary>Parent</summary>
                 <ul class="bg-base-100 rounded-t-none p-2">
+                  
                   <li ><a>Link 1</a></li>
                   <li ><a>Link 2</a></li>
                 </ul>
