21b83468f8ab9f9d50ba5cd07d130683bfc71785
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-27 14:44:56 +0000

    Update DishCard button class to use btn-wide instead of btn-xs for quantity display

--- src/components/card/DishCard.vue
+++ src/components/card/DishCard.vue
@@ -33,7 +33,7 @@ const emit = defineEmits(['add', 'remove'])
       <div class="font-semibold whitespace-nowrap">{{ Number(dish.price).toFixed(2) }}€</div>
 
       <div class="flex items-center gap-2">
-        <span v-if="dish.quantity > 0" class="rounded-full btn px-3 text-sm btn-soft btn-xs ">
+        <span v-if="dish.quantity > 0" class="rounded-full btn px-3 text-sm btn-soft btn-wide ">
           {{ dish.quantity }}
         </span>
 
